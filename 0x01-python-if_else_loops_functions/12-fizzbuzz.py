cd912a99e09f57740a66b3b2f337d064d8c9b3b4
Zevrov <719@holbertonschool.com>
2019-05-01 22:02:20 +0000

    more permission/typo fixes

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for z in range (1, 101):
+    for z in range(1, 101):
         if z % 3 == 0 and z % 5 == 0:
             print('FizzBuzz ', end='')
         elif z % 5 == 0:

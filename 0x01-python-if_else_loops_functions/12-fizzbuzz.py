45e10856ad49549a6ce6399e8cad265e6efc47ba
Zevrov <719@holbertonschool.com>
2019-05-01 02:28:42 +0000

    forgot a colon

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for z in range (1, 101):
-        if z % 15 == 0:
+        if z % 3 == 0 and z % 5 == 0:
             print('FizzBuzz ', end='')
         elif z % 5 == 0:
             print('Buzz ', end='')
         elif z % 3 == 0:
             print('Fizz ', end='')
-        else
+        else:
             print('{:d} '.format(z), end='')

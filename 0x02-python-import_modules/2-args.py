5a2b9ed6c6fb277c4c073d621bb2315b7efd9ff7
Zevrov <719@holbertonschool.com>
2019-05-02 22:52:52 +0000

    typo in line 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ == 'main':
+if __name__ == '__main__':
     from sys import argv
     if len(sys.argv) == 1:
         print('{} arguments.'.format(len(sys.argv) - 1))

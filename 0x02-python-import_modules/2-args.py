21287534d7b923feb07ec6bd7c3a411ced58fc86
Zevrov <719@holbertonschool.com>
2019-05-02 22:54:07 +0000

    test 2 with print formatting on the last line

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     else:
         print('{} arguments:'.format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
-        print('{}: {}'.format(i, sys.argv[z]))
+        print('{:d}: {:s}'.format(i, sys.argv[z]))

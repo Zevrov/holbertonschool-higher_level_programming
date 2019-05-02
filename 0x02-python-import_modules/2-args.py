aa17072e25e222beca8af1cbbc1c0715fd329ec3
Zevrov <719@holbertonschool.com>
2019-05-02 22:58:13 +0000

    Mark helped me a bit here but I had to find a different way to do this kinda

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     from sys import argv
-    if len(sys.argv) == 1:
-        print('{} arguments.'.format(len(sys.argv) - 1))
-    elif len(sys.argv) == 2:
-        print('{} argument:'.format(len(sys.argv) - 1))
+    if len(argv) == 1:
+        print('{} arguments.'.format(len(argv) - 1))
+    elif len(argv) == 2:
+        print('{} argument:'.format(len(argv) - 1))
     else:
-        print('{} arguments:'.format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print('{:d}: {:s}'.format(i, sys.argv[z]))
+        print('{} arguments:'.format(len(argv) - 1))
+    for i in range(1, len(argv)):
+        print('{:d}: {:s}'.format(i, argv[i]))

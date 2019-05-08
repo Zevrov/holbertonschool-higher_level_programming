a28488692a75b7cbdfea69a9576bdd73bde143e7
Zevrov <719@holbertonschool.com>
2019-05-08 03:12:48 +0000

    learned about sorting from here https://wiki.python.org/moin/HowTo/Sorting

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) is None:
+    if len(my_list) == 0:
         return None
-    max = 0
-    for z in my_list:
-        if x < z:
-            x = z
-    return x
+    my_list.sort()
+    my_list.reverse()
+    return my_list[0]

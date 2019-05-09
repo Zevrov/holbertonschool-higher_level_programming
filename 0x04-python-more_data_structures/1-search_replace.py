2c4212177123bb932584a123c60ed96245a65475
Zevrov <719@holbertonschool.com>
2019-05-09 18:37:39 +0000

    got this one without kay's help but the building blocks are working

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-	
+    new_list = []
+    for element in my_list:
+        if element == search:
+            element = replace
+            new_list.append(element)
+        else:
+            new_list.append(element)
+    return new_list

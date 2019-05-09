a13ec64b0ddfa84a7bc4d3e0c3c9814b9c71f308
Wolf Hopkins <zevrov.719@gmail.com>
2019-05-09 14:08:11 -0400

    test for 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+for eachList in matrix:
+	for eachNumber in eachList:
+		matrix.append(square(eachNumber))
+			return matrix
 
-
-
-def square(matrix=[]):
-	
+def square(x):
+	x ** 2
+	return x

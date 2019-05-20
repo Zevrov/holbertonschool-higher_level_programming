128037e3694f98a7aa96e682c65635a7d479843d
Zevrov <719@holbertonschool.com>
2019-05-20 16:52:20 +0000

    found a solution with less lines

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,8 +4,7 @@ from sys import stderr
 
 def safe_function(fct, *args):
     try:
-        function = fct(*args)
+        return fct(*args)
     except Exception as err:
         stderr.write("Exception: {}\n".format(err))
         return (None)
-    return (function)

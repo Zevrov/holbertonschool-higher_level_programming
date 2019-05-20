1733f59b614bc3d11ecdd4f3e01040ecc1a87fd2
Zevrov <719@holbertonschool.com>
2019-05-20 15:58:44 +0000

    bad code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-for x in my_list:
-	try:
-		print("{}".format(my_list[x]))
-	except:
-		pass
+    for i in my_list[i]:
+        try:
+            print("{:d}".format(i))
+        except:
+            pass

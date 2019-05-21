01e038dbd369a75183ea6613375bb8799885d7e2
Zevrov <719@holbertonschool.com>
2019-05-21 18:32:28 +0000

    testing again

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,5 +6,5 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-	def area(self):
-		return self.__size ** 2
+    def area(self):
+        return self.__size ** 2
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 class Square:
-	@property
-	def size(self):
-		return self.__size
-	@size.setter
-	def size(self, value):
-		if not isinstance(value, int)
-			raise TypeError("size must be an integer")
-	    if value < 0:
-			raise ValueError("size must be >= 0")
-		self.__size = value
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
-	def area(self):
-		return self.__size ** 2
+        else:    
+            self.__size = size
+    def area(self):
+        return self.__size ** 2

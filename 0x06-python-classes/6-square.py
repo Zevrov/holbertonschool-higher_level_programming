65a1fe09e90c7287475e076964ef253b013eaca6
Zevrov <719@holbertonschool.com>
2019-05-22 00:02:34 +0000

    got rid of extraneous test cases

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -64,10 +64,6 @@ class Square:
 
         """
 
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
         self.__position = position
 

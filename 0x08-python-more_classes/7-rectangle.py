ae5b6a90f09b864f9d091a2b93293904970e8d24
Wolf Hopkins <zevrov.719@gmail.com>
2019-05-28 15:19:37 -0400

    7v2, accidentally also did 8

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -80,14 +80,3 @@ class Rectangle:
         """kill the Rectangle, decrease instance count"""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """Returns the larger rectangle after comparing"""
-        if type(rect_1) != Rectangle:
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if type(rect_2) != Rectangle:
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_2.area() >= rect_2.area():
-            return rect_2
-        return rect_1

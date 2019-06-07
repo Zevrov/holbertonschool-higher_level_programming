73e60a156484acba8095ae0c70f6c100651543d2
Wolf Hopkins <zevrov.719@gmail.com>
2019-06-06 23:14:54 -0400

    12 test 1

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""method for student creation"""
+
+
+class Student:
+    """Student obj"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        if isinstance(attrs, list):
+            for things in attrs:
+                if not isinstance(attrs, list):
+                    return self.__dict__
+        else:
+            return self.__dict__
+        new_dictionary = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new_dictionary.update({key, value})
+        return new_dictionary

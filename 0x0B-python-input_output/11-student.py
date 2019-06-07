f2483cdb7cf99c0b1844e0e465aab1988109a5d2
Wolf Hopkins <zevrov.719@gmail.com>
2019-06-06 23:09:05 -0400

    pep8 errors

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -6,11 +6,9 @@ class Student:
     """Student obj"""
 
     def __init__(self, first_name, last_name, age):
-    """Set age, and name of student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-    """return dic of instance"""
         return self.__dict__

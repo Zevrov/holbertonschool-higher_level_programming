6685b17742825c05775e4f6c951ca443184ba4d8
Wolf Hopkins <zevrov.719@gmail.com>
2019-06-06 23:22:09 -0400

    update for comments

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -3,7 +3,8 @@
 
 
 class Student:
-    """Student obj"""
+    """Student obj, interesting how you don't have to directly
+    test for strings in a loop, python is weird"""
 
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name

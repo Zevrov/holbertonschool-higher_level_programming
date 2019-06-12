0589e24dea7c933d0ab4d9aa8a8f203136bec8b6
Wolf Hopkins <zevrov.719@gmail.com>
2019-06-12 11:17:01 -0400

    Base is now pep8 approved

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,10 +2,11 @@
 
 
 class Base:
-	def __init__(self, id=None):
-	__nb_objects = 0
-		if id is not None:
-			self.id = id
-		else:
-			Base.__nb_objects += 1
-			self.id = base.__nb_objects
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

eff62e99d68615f7172f7f83b4f84e07514c312a
Wolf Hopkins <zevrov.719@gmail.com>
2019-06-12 14:04:13 -0400

    update to test files

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,6 +2,3 @@
 """Unittests for base model."""
 import unittest
 from models.base import Base
-
-
-class Test 
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+import unittest
+from models.square import Square

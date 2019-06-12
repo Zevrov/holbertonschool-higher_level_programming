7555db5d2e178067efb568a2429c9c73dc61a71b
Wolf Hopkins <zevrov.719@gmail.com>
2019-06-12 12:53:13 -0400

    testing vs checker

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Square model"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class for square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialize the square"""
+        super().__init__(size, size, x, y, id)
+    @property
+    def size(self):
+        """size getter"""
+        return self.__height
+
+    @size.setter
+    def size(self, value):
+        """size setter"""
+        self.__width = value
+        self.__height = value
+
+    def __str__(self):
+        """returns information about the shape"""
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                                 self.x,
+                                                                 self.y,
+                                                                 self.height)

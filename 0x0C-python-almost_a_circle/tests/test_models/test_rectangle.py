825e5684e50387388743c6d709f7e1ef97bb8b3b
Wolf Hopkins <zevrov.719@gmail.com>
2019-06-12 22:52:29 -0400

    rip

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-"""Unittests for rectangle model"""
-import unittest
-from models.rectangle import Rectangle
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-"""Unittests for square model"""
-import unittest
-from models.square import Square

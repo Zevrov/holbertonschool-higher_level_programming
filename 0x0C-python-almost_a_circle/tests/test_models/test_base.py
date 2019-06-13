9e2e0aa886ad3f1df81be5df6c19624baffb178f
Wolf Hopkins <zevrov.719@gmail.com>
2019-06-12 22:45:47 -0400

    testing to see if my unittests work

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,3 +2,24 @@
 """Unittests for base model"""
 import unittest
 from models.base import Base
+
+
+class test_base(unittest.TestCase):
+    """unit testing"""
+    def setup(self):
+        """setup"""
+        importlib.reload(models.Base)
+        importlib.reload(models.Rectangle)
+        importlib.reload(models.Square)
+
+    def None(self):
+        """testing for no input"""
+        x = Base(None)
+        y = Base()
+        self.assertEqual(x.id, 1)
+        self.assertEqual(y.id, 2)
+
+    def Nargs(self):
+        """testing for no arguments"""
+        x = Base()
+        x = self.assertEqual(x.id, 1)

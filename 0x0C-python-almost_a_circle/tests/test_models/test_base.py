a23ec53625f8eae5c2a40c04106549c7fc079972
Wolf Hopkins <zevrov.719@gmail.com>
2019-06-12 22:52:09 -0400

    maybe it works now?

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Unittests for base model"""
 import unittest
+import json
 from models.base import Base
 
 
@@ -23,3 +24,25 @@ class test_base(unittest.TestCase):
         """testing for no arguments"""
         x = Base()
         x = self.assertEqual(x.id, 1)
+
+    def Margs(self):
+        """testing too many arguments"""
+        x = Base()
+        self.assertEqual(x.id, 1)
+        self.assertRaises(TypeError, Base, 23, 43)
+        x = Base()
+        self.assertEqual(b.id, 2)
+
+    def json(self):
+        """json test"""
+        x = Base()
+        self.assertEqual(x.id, 1)
+
+    def id(self):
+        """test for id"""
+        x = Base()
+        self.assertEqual(x.id, 1)
+        y = Base(14)
+        self.assertEqual(y.id, 14)
+        z = Base()
+        self.assertEqual(z.id, 2)

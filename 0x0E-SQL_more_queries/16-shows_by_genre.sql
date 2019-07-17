a32a55040974696741c34864a498ddf9962b0c55
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-16 21:18:50 -0400

    rest of the mandatory for testing vs checker

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- list all shows + all genres linked to that show
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;

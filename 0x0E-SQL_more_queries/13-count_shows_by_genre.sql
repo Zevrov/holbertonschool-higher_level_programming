49f6fddfc9848aea171e16e77c5fe7d119127884
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-16 21:22:12 -0400

    fix typo for 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,7 +3,6 @@ SELECT tv_genres.name AS genre, COUNT(*)
 AS number_shows
 FROM tv_show_genres
 INNER JOIN tv_genres
-
 ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
 ORDER BY number_shows DESC;

b2b2cf012018a8b1b1ea17b4712c60b6ab6e9c4c
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-16 21:23:45 -0400

    testing a fix

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,5 @@
 -- lists all genres + # of shows per
-SELECT tv_genres.name AS genre, COUNT(*)
-AS number_shows
+SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
 FROM tv_show_genres
 INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id

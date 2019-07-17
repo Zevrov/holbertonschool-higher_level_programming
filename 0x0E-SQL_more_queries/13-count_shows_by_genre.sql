87e811bf23a28805b00340280704b03004b18e57
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-16 21:25:28 -0400

    found where my output was going wrong...

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- lists all genres + # of shows per
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
-FROM tv_show_genres
-INNER JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_genres.name
-ORDER BY number_shows DESC;
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id)
+AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre ORDER BY number_of_shows DESC;

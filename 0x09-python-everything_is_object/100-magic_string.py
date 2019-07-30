838ca58bd948999974087392c2bc0b77741cd1ec
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-30 03:58:30 +0200

    test

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    string = set((0,))
-    string.add(s.pop() + 1)
-    return ', '.join('Holberton' for index in range(next(iter(string))))
+def magic_string(Holby=[]):
+    Holby += ["Holberton"]
+    return (", ".join(Holby))

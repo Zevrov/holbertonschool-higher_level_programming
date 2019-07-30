468e33a20e2e06c71e3087ee78f94f7f260cd00c
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-30 03:55:24 +0200

    got rid of whitespace

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def magic_string():
-    string=set((0,))
+    string = set((0,))
     string.add(s.pop() + 1)
     return ', '.join('Holberton' for index in range(next(iter(string))))

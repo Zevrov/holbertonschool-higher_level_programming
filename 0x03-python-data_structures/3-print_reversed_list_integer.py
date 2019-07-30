0436739fba1d3aa67381adad71a51e02c56a2b9e
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-30 03:00:45 +0200

    fixed typo

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list[]):
-    if not my_list:
-        return None
-    for index in reversed(my_list):
-        print("{:d}".format(index))
+	if not my_list:
+		return None
+	for index in reversed(my_list):
+		print("{:d}".format(index))

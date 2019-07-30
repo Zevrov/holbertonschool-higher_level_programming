71bb620a3b72650860494e0f3f110ac6d43f2a40
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-30 04:13:11 +0200

    typo

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -4,7 +4,6 @@
 import json
 import os.path
 import sys
-from sys import argv
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file

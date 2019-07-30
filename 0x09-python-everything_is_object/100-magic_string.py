b838eba04f146bebaea354e12d0aa55d151d0f49
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-30 03:52:47 +0200

    oops forgot to return

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -2,4 +2,4 @@
 def magic_string():
     setattr(magic_string, "x", getattr(magic_string, "x", -1) + 1)
     ret = ("Holberton" + ", Holberton" * getattr(magic_string, "x", 0))
-    return
+    return ret

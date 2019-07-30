ce8c5f79ab98d9e48e29e19be99cfce919e6e2ba
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-30 03:49:49 +0200

    test

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def magic_string():
+    setattr(magic_string, "x", getattr(magic_string, "x", -1) + 1)
+    ret = "Holberton" + ", Holberton" * getattr(magic_string, "x", 0)
+    return

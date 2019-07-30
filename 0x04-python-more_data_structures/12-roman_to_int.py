ce466bca17e72190958c0bb1b9650f96f859fffb
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-30 03:38:04 +0200

    test

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,12 +3,11 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
 
+    numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
     sum = 0
-    numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     for index in reversed(roman_string):
         num = numbers[index]
-        if sum < num * 5 else -num:
-            sum += num
+        sum += num if sum < num * 5 else -nums
     return sum

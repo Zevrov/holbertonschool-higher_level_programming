071abdcf0c48b275bf81dc1d26990c74df3e709d
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-30 04:12:31 +0200

    test

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Module for saving to json'''
+
+import json
+import os.path
+import sys
+from sys import argv
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+file = "add_item.json"
+json_list = []
+
+if os.path.exists(file):
+    json_list = load_from_json_file(file)
+
+for arg in argv[1:]:
+    json_list.append(arg)
+
+save_to_json_file(json_list, file)

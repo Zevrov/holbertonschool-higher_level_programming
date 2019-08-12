e707b3f2f4b9baef20d9500330a57c21710e26cf
Wolf Hopkins <719@holbertonschool.com>
2019-08-11 23:29:05 -0400

    forgot to push city model

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+
+
+Base = declarative_base()
+
+
+class City(Base):
+
+    def __init(self):
+        pass
+
+    __tablename__ = 'cities'
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)

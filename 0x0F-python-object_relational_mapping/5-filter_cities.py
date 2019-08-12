97467a865351ffc82a17dc217f708cfe1984e0f4
Wolf Hopkins <719@holbertonschool.com>
2019-08-11 22:41:27 -0400

    typo correction

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,11 +9,10 @@ if __name__ == "__main__":
     database = MySQLdb.connect(user=argv[1],
                                passwd=argv[2],
                                db=argv[3])
-
     curs = database.cursor()
     curs.execute("SELECT cities.name FROM cities INNER JOIN states ON \
                   cities.state_id=states.id WHERE states.name LIKE %s \
-                  ORDER BY cities.id ASC", [sys.argv[4]])
+                  ORDER BY cities.id ASC", [argv[4]])
     rows = curs.fetchall()
     s = []
     print(", ".join(row[0] for row in rows))

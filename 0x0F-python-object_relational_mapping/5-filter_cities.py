ed54f305538252d1684d4c0027994ee06a45eeaa
Wolf Hopkins <719@holbertonschool.com>
2019-08-11 22:41:16 -0400

    test

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(user=argv[1],
+                               passwd=argv[2],
+                               db=argv[3])
+
+    curs = database.cursor()
+    curs.execute("SELECT cities.name FROM cities INNER JOIN states ON \
+                  cities.state_id=states.id WHERE states.name LIKE %s \
+                  ORDER BY cities.id ASC", [sys.argv[4]])
+    rows = curs.fetchall()
+    s = []
+    print(", ".join(row[0] for row in rows))
+    curs.close()
+    database.close()

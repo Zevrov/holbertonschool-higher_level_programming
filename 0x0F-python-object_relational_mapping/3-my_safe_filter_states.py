c7fa789289aa96e540a077b831384a99228a7356
Wolf Hopkins <719@holbertonschool.com>
2019-08-11 22:30:01 -0400

    fixed pep8

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,8 +6,8 @@ from sys import argv
 
 if __name__ == "__main__":
     database = MySQLdb.connect(user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+                               passwd=argv[2],
+                               db=argv[3])
     curs = database.cursor()
     curs.execute("SELECT * FROM states WHERE name\
                   LIKE %s ORDER BY states.id\

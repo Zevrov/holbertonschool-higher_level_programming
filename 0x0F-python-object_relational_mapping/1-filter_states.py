8126ce421c0f2f91512f697caaa32665263524df
Wolf Hopkins <719@holbertonschool.com>
2019-08-11 21:52:38 -0400

    getting states twice, might've executed multiple times

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,11 +10,10 @@ if __name__ == "__main__":
     database = MySQLdb.connect(user=argv[1],
                                passwd=argv[2],
                                db=argv[3])
-    curs = db.cursor()
-    curs.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY \
-                  states.id ASC")
+    curs = database.cursor()
+    curs.execute('SELECT * FROM `states` WHERE `name` LIKE BINARY "N%%";')
     rows = curs.fetchall()
     for row in rows:
         print(row)
     curs.close()
-    databaseb.close()
+    database.close()

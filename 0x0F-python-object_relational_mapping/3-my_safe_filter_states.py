bdf562f84c8641d5783daaa1e0de54c296a9c7fe
Wolf Hopkins <719@holbertonschool.com>
2019-08-11 22:29:09 -0400

    test

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    curs = database.cursor()
+    curs.execute("SELECT * FROM states WHERE name\
+                  LIKE %s ORDER BY states.id\
+                  ASC", [argv[4]])
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+    curs.close()
+    database.close()

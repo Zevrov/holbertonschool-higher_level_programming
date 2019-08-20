a45fc3f9ef8afea8f4ea21398c65135a38d4a07e
Wolf Hopkins <719@holbertonschool.com>
2019-08-20 18:51:25 -0400

    typo in the position of the argument

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A header variable X-HolbertonSchool-User-Id must be sent with the value 98
-curl -s -H "$1" 'X-HolbertonSchool-User-Id: 98'
+curl -s -H 'X-HolbertonSchool-User-Id: 98' "$1"

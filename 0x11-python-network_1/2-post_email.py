eddef3c3d351e920eb5481a0e6f585656e7cf535
Wolf Hopkins <719@holbertonschool.com>
2019-08-26 19:51:17 -0400

    fixed a formatting typo in 2, line 10 + 11

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,6 +7,5 @@ import sys
 
 if __name__ == '__main__':
     with urllib.request.urlopen(sys.argv[1],
-                                b'email=' + sys.argv[2].encode(),)
-    as req:
-        print(req.read().decode('UTF-8'))
+                                b'email=' + sys.argv[2].encode()) as req:
+                                    print(req.read().decode('UTF-8'))

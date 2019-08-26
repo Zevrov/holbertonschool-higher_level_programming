c500c9309647fd2719c22e5c082c7b1dc25e02fb
Wolf Hopkins <719@holbertonschool.com>
2019-08-26 19:47:19 -0400

    added file for exercise 2, added rwx permissions

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""send a post request to a url, show response after"""
+
+import urllib.request
+import sys
+
+
+if __name__ == '__main__':
+    with urllib.request.urlopen(sys.argv[1],
+                                b'email=' + sys.argv[2].encode(),)
+    as req:
+        print(req.read().decode('UTF-8'))

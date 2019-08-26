3ceb972d15d967d1bbe7566b4e4353c54d337fd0
Wolf Hopkins <719@holbertonschool.com>
2019-08-26 19:55:48 -0400

    added file for exercise 3, added rwx permissions

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""send a web req with error handling"""
+
+import urllib.request
+import sys
+
+
+if __name__ == '__main__':
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('UTF-8'))
+    except urllib.error.HTTPError as error:
+        print('Error code: {}', format.(error.code))

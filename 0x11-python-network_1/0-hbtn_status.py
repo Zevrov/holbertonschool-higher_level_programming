1833e43cf07ed52afaf3033eb835299cf1d5c624
Wolf Hopkins <719@holbertonschool.com>
2019-08-26 19:26:23 -0400

    got rid of if __main__

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,11 +6,10 @@ import urllib.request
 url = 'https://intranet.hbtn.io/status'
 
 
-if __name__ == "__main__":
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        type_html = type(html)
-        print("Body response:")
-        print("\t- type: {}".format(type_html))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+    type_html = type(html)
+    print("Body response:")
+    print("\t- type: {}".format(type_html))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))

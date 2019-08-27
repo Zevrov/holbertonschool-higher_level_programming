304e24797f20d106cd8f3ab3666c7823c05dc571
Wolf Hopkins <719@holbertonschool.com>
2019-08-26 20:57:04 -0400

    added exercise 9, added rwx permissions to 9

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that sends a search req to Star Wars api"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    search = requests.get(
+        'https://swapi.co/api/people/',
+        params={'search': sys.argv[1]}
+    )
+    response = search.json
+    print('Number of results:', response.get('count'))
+    for thing in response.get('results'):
+        print(thing.get('name'))

cecc2a995b2ba2c88769fad975c48c332b5ed69c
Wolf Hopkins <719@holbertonschool.com>
2019-08-26 20:17:01 -0400

    fixing syntax error on line 10

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,5 +7,5 @@ import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    with = requests.get(url) as response:
+    with requests.get(url) as response:
         print(response.headers.get("X-Request-Id"))

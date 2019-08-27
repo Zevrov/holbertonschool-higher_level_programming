00391009a09808097e73cedcdea233b212b91bc5
Wolf Hopkins <719@holbertonschool.com>
2019-08-26 21:04:13 -0400

    adding exercise 10, adding rwx permissions to 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script to get the id of an authenticated github user"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    search = requests.get(
+        "https://api.github.com/user",
+        auth=(sys.argv[1], sys.argv[2])
+    )
+    print(search.json().get("if"))

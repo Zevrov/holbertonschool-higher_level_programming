eca2f2bb66c1a5b10f39266158fc3623f4f5739f
Wolf Hopkins <719@holbertonschool.com>
2019-08-26 20:49:53 -0400

    added body to exercise 8, added rwx permissions to 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""search an API"""
+
+import requests
+import sys
+url = "http://0.0.0.0:5000/search_user"
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        search = {'q': '""'}
+     else:
+        search = {'q': sys.argv[1]}
+    request = requests.post(url, search)
+    try:
+        search = request.json()
+    except ValueError:
+        print("Not a valid JSON")
+    else:
+        if hasattr(search, '__contains__') and len(search) < 1:
+            print('No result')
+        else:
+            print('[{}] {}'.format(search['id'], search['name']))

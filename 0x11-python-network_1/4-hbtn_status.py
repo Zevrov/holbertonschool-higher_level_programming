9a25b8c8d56c3b5d79b1e3da54fb073c313084db
Wolf Hopkins <719@holbertonschool.com>
2019-08-26 20:10:06 -0400

    fix to format on response variable.

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,7 +8,6 @@ url = 'https://intranet.hbtn.io/status'
 
 if __name__ == '__main__':
     req = requests.get(url)
-    response = response.text
     print("Body response:")
-    print("\t- type: {}".format(type(response)))
-    print("\t- content: {}".format(response))
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))

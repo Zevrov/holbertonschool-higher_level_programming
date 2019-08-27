a89edb81681077d482638aa28aaee5a7d2dab428
Wolf Hopkins <719@holbertonschool.com>
2019-08-26 20:42:44 -0400

    added exercise 7, added rwx permissions for 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""send a web req with error handling"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    response = requests.get(sys.argv[1])
+    if response.status_code > 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)

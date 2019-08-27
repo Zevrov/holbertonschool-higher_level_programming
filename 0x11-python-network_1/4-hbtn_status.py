db6b8b88f6a8a9aaece17d9799c1d0a0d71400f4
Wolf Hopkins <719@holbertonschool.com>
2019-08-26 20:06:08 -0400

    added file for exercise 4, added rwx permissions

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""url fetching script in python"""
+
+import requests
+
+url = 'https://intranet.hbtn.io/status'
+
+
+if __name__ == '__main__':
+    req = requests.get(url)
+    response = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(response))
+    print("\t- content: {}".format(response))

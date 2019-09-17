cc0e26632ed1c2024448e72500c488d9bf7312b6
Wolf Hopkins <719@holbertonschool.com>
2019-09-17 14:53:32 -0400

    testing a fix for 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 
 const process = require('process');
 const args = process.argv;
-if (args.length <= 3) {
+if (args.length < 4) {
   console.log(0);
 } else {
   console.log(args.sort().reverse()[1]);

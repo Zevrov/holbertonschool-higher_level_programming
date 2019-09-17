d17dfcf589d32f3745c8678c4f2508617f0e91ca
Wolf Hopkins <719@holbertonschool.com>
2019-09-17 14:38:47 -0400

    finished task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const process = require('process');
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+function add (a, b) {
+  return (a + b);
+}
+console.log(add(a, b));

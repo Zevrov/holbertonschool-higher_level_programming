a760396b121e2489b3c96849e3a2d7e1dc35bead
Wolf Hopkins <719@holbertonschool.com>
2019-09-17 14:12:25 -0400

    finished task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const process = require('process');
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
+}

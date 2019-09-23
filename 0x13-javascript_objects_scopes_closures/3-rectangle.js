0f6ae114152a903c346c8690d2a3d798456bce13
Wolf Hopkins <719@holbertonschool.com>
2019-09-23 19:47:56 -0400

    testing solution to 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (x, y) {
+    if (x > 0 && y > 0) {
+      this.width = x;
+      this.height = y;
+    }
+  }
+};
+
+  print () {
+    const array [];
+    for (let index = 0; index < this.width; index++) {
+      array.push('X');
+    }
+    for (let index = 0; index < this.height; index++) {
+      console.log(array.join(''));
+    }
+}
+};

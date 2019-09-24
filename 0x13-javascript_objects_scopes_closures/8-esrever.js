96d1c58b75c892d96fe6e208c2dab04e2dd4f14c
Wolf Hopkins <719@holbertonschool.com>
2019-09-23 20:17:00 -0400

    testing solution to 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const ret = Array(list.length);
-  for (let index = list.length - 1; index >= 0; index--) {
-    ret.push(ret[index]);
+  let count = 0;
+  for (let index = list.length - 1, count = 0; index >= 0; index--, count++) {
+    ret[count] = list[index];
   }
   return ret;
 };

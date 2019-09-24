a0a457e1b582571f35bb336f31de5e480e81bef1
Wolf Hopkins <719@holbertonschool.com>
2019-09-23 20:17:56 -0400

    solution for 8 removed a bad line on line 4

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const ret = Array(list.length);
-  let count = 0;
   for (let index = list.length - 1, count = 0; index >= 0; index--, count++) {
     ret[count] = list[index];
   }

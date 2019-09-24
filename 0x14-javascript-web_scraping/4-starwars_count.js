6598707c39b213a0c411690de4faf8ac7449dc64
Wolf Hopkins <719@holbertonschool.com>
2019-09-24 17:43:04 -0400

    finished task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+request(url, (error, response, body) => {
+  if (error) { return; }
+  let count = 0;
+  const result = JSON.parse(body).results;
+  for (const movie of result) {
+    for (const character of movie.characters) {
+      if (character.includes("/18/")|| character.includes("/18")) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+});
